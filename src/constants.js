972c1b140234f2fe07c7f1c3aca07ed450c6493f
kinhyo97 <v50509v@gmail.com>
2025-11-18 14:40:55 +0900

    dd

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+//url 상수선언
+
+//export const REST_URL = 'http://localhost:8080/'
+//export const REST_URL = 'http://3.36.216.187:8080/'
+export const REST_URL = 'http://ec2-3-36-216-187.ap-northeast-2.compute.amazonaws.com:8080/' // 개방 주소법
+
