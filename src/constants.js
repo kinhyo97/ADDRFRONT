19884763895580e988dc59e01b05b0e988947740
kinhyo97 <v50509v@gmail.com>
2025-11-18 16:05:40 +0900

    constants 수정

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,6 @@
 
 //export const REST_URL = 'http://localhost:8080/'
 //export const REST_URL = 'http://3.36.216.187:8080/'
-export const REST_URL = 'http://ec2-3-36-216-187.ap-northeast-2.compute.amazonaws.com:8080/' // 개방 주소법
+//export const REST_URL = 'http://ec2-3-36-216-187.ap-northeast-2.compute.amazonaws.com:8080/' // 개방 주소법
+export const REST_URL = 'https://ec2-3-36-216-187.ap-northeast-2.compute.amazonaws.com:443/' // 개방 주소법
 
